fc9695a8a75525c0b33f2c6b83e962649a0b2f31
Narcssus <narcssusinnook@163.com>
2016-02-04 21:25:57 +0800

    基础不牢就是不行啊~
    看到标签是DFS就方了。。。。二叉树的先根遍历就是DFS！二叉树的先根遍历就是DFS！二叉树的先根遍历就是DFS！二叉树的先根遍历就是DFS！

--- src/Q113/Solution.java
+++ src/Q113/Solution.java
@@ -7,7 +7,7 @@ class TreeNode {
 	int val;
 	TreeNode left;
 	TreeNode right;
-	
+
 	TreeNode(int x) {
 		val = x;
 	}
@@ -16,43 +16,43 @@ class TreeNode {
 
 public class Solution {
 	public static List<List<Integer>> pathSum(TreeNode root, int sum) {
-		List<List<Integer>> ans=new ArrayList<List<Integer>>();
-		List<Integer> tmp =new ArrayList<Integer>();
+		List<List<Integer>> ans = new ArrayList<List<Integer>>();
+		List<Integer> tmp = new ArrayList<Integer>();
 		GetPath(root, sum, ans, tmp);
 		return ans;
 	}
-	
-	
-	public static void GetPath(TreeNode root, int sum,List<List<Integer>> ans,List<Integer> tmp){
-		if(root!=null){
-			sum-=root.val;
-			tmp.add(root.val);
-			if(sum==0&&root.right==null&&root.left==null) {
-				ans.add(tmp);
-				tmp=new ArrayList<Integer>();
-			}
-			else if(sum<0){
-				sum+=root.val;
-				tmp.remove(tmp.size()-1);
-			}
-			
+
+	public static void GetPath(TreeNode root, int sum, List<List<Integer>> ans,
+			List<Integer> tmp) {
+		if (root == null)
+			return;
+
+		
+		tmp.add(root.val);
+		sum -= root.val;
+		if (sum == 0 && root.right == null && root.left == null) {
+			ans.add(new ArrayList<Integer>(tmp));
+			tmp.remove(tmp.size() - 1);
+			return;
 		}
-		
-		if(root!=null&&root.left!=null) GetPath(root.left,sum,ans,tmp);
-		if(root!=null&&root.right!=null) GetPath(root.right,sum,ans,tmp);
+
+		if (root != null && root.left != null)
+			GetPath(root.left, sum, ans, tmp);
+		if (root != null && root.right != null)
+			GetPath(root.right, sum, ans, tmp);
+		tmp.remove(tmp.size() - 1);
 	}
-	
-	public static void main(String[] args){
-		
-		TreeNode a=new TreeNode(1);
-		TreeNode b=new TreeNode(2);
-		TreeNode c=new TreeNode(3);
-		a.left=b;
-		a.right=c;
-		List<List<Integer>> ans=pathSum(a,1);
-		System.out.println(ans.toString());
-		
-	}
-	
-	
+
+//	public static void main(String[] args) {
+//
+//		TreeNode a = new TreeNode(1);
+//		TreeNode b = new TreeNode(2);
+//		TreeNode c = new TreeNode(3);
+//		a.left = b;
+//		a.right = c;
+//		List<List<Integer>> ans = pathSum(a, 3);
+//		System.out.println(ans.toString());
+//
+//	}
+
 }
