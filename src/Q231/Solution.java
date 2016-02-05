4b0ae4ae45cb51256ca221a39d56feb7ca762a21
Narcssus <narcssusinnook@163.com>
2016-02-05 23:06:53 +0800

    74. Search a 2D Matrix
    https://leetcode.com/problems/search-a-2d-matrix/
    如果你懒，就不要在意AC rate(逃~~

--- src/Q231/Solution.java
+++ src/Q231/Solution.java
@@ -11,8 +11,8 @@ public class Solution {
         return true;
     }
 
-	public static void main(String[] args) {
-		System.out.print(isPowerOfTwo(0));
-	}
+//	public static void main(String[] args) {
+//		System.out.print(isPowerOfTwo(0));
+//	}
 
 }
