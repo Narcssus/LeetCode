a65f13b7e8eb0e39fc6b871b856de9b3c7d3c3c1
Narcssus <narcssusinnook@163.com>
2016-02-05 14:47:27 +0800

    231. Power of Two
    https://leetcode.com/problems/power-of-two/

---
+++ src/Q231/Solution.java
@@ -0,0 +1,18 @@
+package Q231;
+
+public class Solution {
+
+	public static boolean isPowerOfTwo(int n) {
+        if(n<=0)	return false;
+        while(n!=1){
+            if(n!=(n/2)*2) return false;
+            n=n/2;
+        }
+        return true;
+    }
+
+	public static void main(String[] args) {
+		System.out.print(isPowerOfTwo(0));
+	}
+
+}
