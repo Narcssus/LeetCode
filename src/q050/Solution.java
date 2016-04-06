c65a543db4e548c9f8a708d22e79453913f9c9e1
Narcssus <narcssusinnook@163.com>
2016-04-06 14:33:04 +0800

    50. Pow(x, n)
    https://leetcode.com/problems/powx-n/

---
+++ src/q050/Solution.java
@@ -0,0 +1,18 @@
+package q050;
+
+public class Solution {
+	public double myPow(double x, int n) {
+		if (n == 0)
+			return 1;
+		if(n<0){
+			x=1/x;
+			n=-n;
+		}
+		if (n % 2 == 0) {
+			return myPow(x * x, n / 2);
+		} else {
+			return myPow(x, n - 1) * x;
+		}
+
+	}
+}
